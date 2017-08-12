1dc25577c08148dea37e50e97d8f442b92e65baf
leo <liff1229@gmail.com>
2017-08-12 13:58:40 +0800

    fix example.py

--- example.py
+++ example.py
@@ -4,11 +4,6 @@
 from segmenter import NaiveSegmenter
 
 seg=NaiveSegmenter()
-with open('./data/pku_test.utf8','r') as pku_file:
-	pku_content=pku_file.read()
-
-with open('./data/pku_test.utf8','r') as msr_file:
-	msr_content=msr_file.read()
 
 print seg.cut('8月8日，甘州区法院处置一起哄闹法院，辱骂、撕扯和殴打法官事件，闹事女子张某某被控制后，被公安机关民警带走')
 
